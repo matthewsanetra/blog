2bbf79f05fa139ed363b035ff4ee69db01b298e8
Matthew Sanetra <41018997+matthewsanetra@users.noreply.github.com>
2023-03-13 10:55:12 +0000

    Move Title and Description checks to Zod

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,8 +3,12 @@ import { z, defineCollection } from "astro:content";
 export const collections = {
   blog: defineCollection({
     schema: z.object({
-      title: z.string(),
-      description: z.string(),
+      title: z
+        .string()
+        .max(70, { message: "Title is too long for Twitter card" }),
+      description: z
+        .string()
+        .max(200, { message: "Description is too long for Twitter card" }),
       date: z.date(),
       image: z.string().optional(),
     }),
