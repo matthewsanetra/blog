89acc5c47c998374e45307476252aec0595beb33
Matthew Sanetra <41018997+matthewsanetra@users.noreply.github.com>
2023-03-18 15:14:24 +0000

    Tidy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from "astro/config";
 
-import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
+import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
 import compress from "astro-compress";
