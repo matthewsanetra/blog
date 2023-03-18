340bdb41258f7ebb5c2a45dbc35ae34a62d8ee69
Matthew Sanetra <41018997+matthewsanetra@users.noreply.github.com>
2023-03-18 12:11:55 +0000

    Tidy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
 import { defineConfig } from "astro/config";
+
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
-import compress from "astro-compress";
-
 import sitemap from "@astrojs/sitemap";
+
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://mattsanetra.uk/",
-  integrations: [tailwind(), mdx(), compress(), sitemap()],
+  integrations: [mdx(), tailwind(), sitemap(), compress()],
   markdown: {
     shikiConfig: {
       theme: "github-light",
