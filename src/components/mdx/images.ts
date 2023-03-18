89a0d309d0228284a92cb982889de3590be101c3
Matthew Sanetra <41018997+matthewsanetra@users.noreply.github.com>
2023-03-18 13:32:54 +0000

    Move moomin to assets

---
+++ src/components/mdx/images.ts
@@ -0,0 +1,8 @@
+// Importing assets within MDX files didn't work for me,
+// so I created this file to import them and export them as a module.
+
+import muminekSitting from "@assets/moomins/muminek-sitting.svg";
+
+export default {
+  muminek: muminekSitting,
+};
